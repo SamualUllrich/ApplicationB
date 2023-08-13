8eb3170657efe71541826c67bf7b21a936863b18
Sam <ullrichsam@gmail.com>
2023-08-12 22:40:03 -0700

    Fixed issues with server connection. Added a connection termination feature. Added error conditions detection and warning messages.

--- ApplicationB/MainPage.xaml.cs
+++ ApplicationB/MainPage.xaml.cs
@@ -6,6 +6,7 @@ namespace ApplicationB
     public partial class MainPage : ContentPage
     {
         private ClientWebSocket _webSocket;
+        private bool _isConnected = false;
 
         public MainPage()
         {
@@ -15,15 +16,43 @@ namespace ApplicationB
 
         private async void OnConnectClicked(object sender, EventArgs e)
         {
+
+            if (_webSocket != null && _webSocket.State == WebSocketState.Open)
+            {
+                await DisplayAlert("Connection Error", "Already connected to Application A. Please disconnect before attempting to reconnect.", "OK");
+                return;
+            }
+            
             // Call the REST API from Application A to start streaming
+            _webSocket = new ClientWebSocket();
             var client = new HttpClient();
             var response = await client.GetAsync("http://localhost:9696/api/start");
+
             if (response.IsSuccessStatusCode) 
             {
+                _isConnected = true;
+                ConnectBtn.Text = "Streaming Started";
                 // Connect to the web socket in Application A
                 await _webSocket.ConnectAsync(new Uri("ws://localhost:9696/data"), CancellationToken.None);
                 await ReadDataAsync();
             }
+        }
+
+        private async void OnTerminateClickedAsync(object sender, EventArgs e)
+        {
+            if (!_isConnected)
+            {
+                DisplayAlert("Disconnect Error", "Not currently connected to Application A.", "OK");
+                return;
+            }
+
+            await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+
+            // Dispose of the WebSocket client
+            _webSocket.Dispose();
+            _webSocket = null;
+
+            _isConnected = false;
         }
 
         private async Task ReadDataAsync()
